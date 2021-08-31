56aa42379f13deb069c8b1652c6c03f49681411f
Daniela <daniela.delaserna@gmail.com>
2021-08-31 11:03:47 -0300

    modificacion de to do list

--- index-listas.js
+++ index-listas.js
@@ -1,17 +1,40 @@
 function addItem(){
     const taskValue = document.getElementById("input-task").value;
-    addElemToDom("li", taskValue, "list");
+    addElement(taskValue);
     document.getElementById("input-task").value = "";
 }
 
-function addElemToDom(type, content, parentId){
-    const elem = document.createElement (type);
+/*function addElemToDom(content){
+    const li = document.createElement ("li");
     const text = document.createTextNode (content);
-    const parent = document.getElementById (parentId);
+    const parent = document.getElementById ("list");
 
-    elem.appendChild(text);
-    parent.appendChild(elem);
+    li.appendChild(text);
+    parent.appendChild(li);
 
+
+
+}*/
+function addElement(content){
+const lista = document.getElementById("lista");
+const div = document.createElement("div");
+div.classList.add("list-item");
+
+lista.appendChild(div);
+
+const ul = document.createElement("ul");
+ul.classList.add("ul");
+
+div.appendChild(ul);
+const text = document.createTextNode (content);
+ul.appendChild(text);
+const btn = document.createElement("button");
+
+div.appendChild(btn);
+const btnContent= document.createTextNode("X");
+btn.appendChild(btnContent);
 
 
 }
+
+
